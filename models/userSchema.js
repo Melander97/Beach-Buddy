639a717a7c7c07ca38c8f2656bbb42af02ed81ae
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-05-23 12:11:35 +0200

    fix: local repo, install cookie-parser, validator and add hash password function in userSchema

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,5 +1,7 @@
 // Schema for users
 const mongoose = require("mongoose");
+const {isEmail} = require('validator');
+const bcrypt = require('bcrypt');
 
 //register
 const userSchema = mongoose.Schema(
@@ -12,16 +14,28 @@ const userSchema = mongoose.Schema(
       type: String,
       required: [true, "Please add an email"],
       unique: true,
+      validate: [isEmail, 'Invalid Email']
     },
     password: {
       type: String,
       required: [true, "Please add an password"],
+      minlength: [6, 'Minimum password length is 6 characters'],
     },
   },
-  {
+  { 
     timestamps: true,
-  }
+  } 
 );
+
+// fire a function to hash the password before saving to db
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+
 
 module.exports = mongoose.model("User", userSchema);
 //register end
+
