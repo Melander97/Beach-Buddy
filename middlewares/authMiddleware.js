193629ffb310f86d22410738955454a55d4c516e
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-05-23 12:15:45 +0200

    add  authMiddleware check function

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,34 +5,25 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/userSchema');
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
-
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    try {
-      // Get token from header
-      token = req.headers.authorization.split(' ')[1];
-
-      //Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      //Get user from the token
-      req.user = await User.findById(decoded.id).select('-password');
-
-      next();
-    } catch (error) {
-      console.log(error)
-      res.status(401)
-      throw new Error('Not authorized')
-    }
-  }
-
-  if (!token) {
-    res.status(401);
-    throw new Error('Not authorized, no token ');
+  // Read the token from the cookie
+  const token = req.cookies.jwt;
+  if (!token)
+    return res.status(401).json({
+      success: false,
+      message: "Access denied...No token provided...",
+    });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    // console.log("err", er);
+    //Incase of expired jwt or invalid token kill the token and clear the cookie
+    res.clearCookie("jwt");
+    return res.status(400).json({
+      success: false,
+      message: "Please log in"
+    });
   }
 });
-
 module.exports = { protect }
