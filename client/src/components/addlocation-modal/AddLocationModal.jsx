4db65c65e883ab8365a23593ed7335d1231ff1f3
melander97 <alexandermelander1@gmail.com>
2022-05-27 00:14:44 +0200

    fix

--- client/src/components/addlocation-modal/AddLocationModal.jsx
+++ client/src/components/addlocation-modal/AddLocationModal.jsx
@@ -3,12 +3,13 @@ import './AddLocation-modal.scss';
 
 
 
+
 const AddLocationModal = () => {
   return (
 <div className="pageWrapper">
 
 	{/* Component here */}
-
+    
     <div className="w-full h-screen flex items-center justify-center my-3">
     <div className="bg-[#EDC891] w-96 h-auto rounded-lg pt-8 pb-8 px-8 flex flex-col items-center shadow-xl">
         <button className="w-full h-12 rounded-lg bg-white text-gray-600 uppercase font-semibold hover:bg-gray-100 transition mb-4">Title</button>
@@ -18,7 +19,7 @@ const AddLocationModal = () => {
         <button className="w-full h-12 rounded-lg bg-gray-800 text-gray-200 uppercase font-semibold hover:bg-gray-900 transition mb-0 mt-4">Add Location</button>
     </div>
     </div>
-
+ 
 
 </div>
   )
