ab5a1bb305a61e07b97fb410e5ddf98d47c14739
Naelia Salas <naeliasalas@gmail.com>
2022-05-27 17:51:05 +0200

    change the size of login form to be more fitting to mobile phone

--- client/src/components/login-form/LoginForm.jsx
+++ client/src/components/login-form/LoginForm.jsx
@@ -36,10 +36,10 @@ const LoginForm = () => {
 
 	}
   return (
-    <div className="login--bg grid place-items-center w-full shadow-lg h-5/6">
+    <div className="login--bg grid place-items-center w-full shadow-lg h-5/6 ">
       <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form className="space-y-6 w-80" action="#">
-          <h3 className="text-xl font-medium text-gray-900 dark:text-white text-center">
+        <form className="space-y-6 w-80 space-y-2 w-60 pt-6 " action="#"  >
+          <h3 className="text-xl font-medium text-gray-900 dark:text-white text-center pb-4">
             Sign in
           </h3>
           {data && 
@@ -48,7 +48,7 @@ const LoginForm = () => {
           <div>
             <label
               htmlFor="email"
-              className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+              className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300 py-1"
             >
               Your email
             </label>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    // screens: {
+    //   'xs': '475',
+    //   => @media (min-width: 768px) { ... }
+    // }
   },
   plugins: [],
 };
