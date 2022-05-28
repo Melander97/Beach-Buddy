dede651f8ae188eb8fa11432f20fdcafab05a293
Naelia Salas <naeliasalas@gmail.com>
2022-05-28 15:52:45 +0200

    Replace button to input form in addLocation

--- client/src/components/addlocation-modal/AddLocationModal.jsx
+++ client/src/components/addlocation-modal/AddLocationModal.jsx
@@ -1,23 +1,81 @@
-import React from 'react'
-import './AddLocation-modal.scss'; 
 
 
-
+import React, { useEffect, useState } from "react";
+import "./AddLocation-modal.scss";
 
 const AddLocationModal = () => {
-  return (
+  const [title, setTitle] = useState("");
+  const [adress, setAdress] = useState("");
+  const [directions, setDirection] = useState("");
 
+  return (
+    
     <div className="addLocation-component w-full h-screen flex items-center justify-center my-3">
-    <div className="bg-[#EDC891] w-96 h-auto rounded-lg pt-8 pb-8 px-8 flex flex-col items-center shadow-xl">
-        <button className="w-full h-12 rounded-lg bg-white text-gray-600 uppercase font-semibold hover:bg-gray-100 transition mb-4">Title</button>
-        <button className="w-full h-12 rounded-lg bg-white text-gray-600 uppercase font-semibold hover:bg-gray-100 transition mb-4">Address</button>
-        <button className="w-full h-12 rounded-lg bg-white text-gray-600 uppercase font-semibold hover:bg-gray-100 transition mb-4">Directions</button>
-        <button className="w-full h-12 rounded-lg bg-white text-gray-600 uppercase font-semibold hover:bg-gray-100 transition mb-4">Upload Image</button>
-        <button className="w-full h-12 rounded-lg bg-gray-800 text-gray-200 uppercase font-semibold hover:bg-gray-900 transition mb-0 mt-4">Add Location</button>
+      {/* <div className="w-medium bg-white rounded shadow-lg p-8 m-4"> */}
+      <form className="bg-[#EDC891] w-80 h-auto rounded-lg pt-8 pb-8 px-6 flex flex-col items-center shadow-xl mb-4 md:flex md:flex-wrap md:justify-between">
+        <div>
+          <label
+            htmlFor="title"
+            className="text-sm font-medium text-gray-900 block mb-2 dark:text-black-300 py-1"
+          >
+            Namn
+          </label>
+          <input
+            type="title"
+            name="title"
+            id="title"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            className="w-full h-12 rounded-lg bg-white text-gray-600 uppercase font-semibold hover:bg-gray-100 transition mb-4 "
+            // placeholder="Namn"
+            required=""
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="title"
+            className="text-sm font-medium text-gray-900 block mb-2 dark:text-black-300 py-1"
+          >
+            Plats
+          </label>
+          <input
+            type="adress"
+            name="adress"
+            id="adress"
+            onChange={(e) => setAdress(e.target.value)}
+            value={adress}
+            className="w-full h-12 rounded-lg bg-white text-gray-600 uppercase font-semibold hover:bg-black-100 transition mb-4"
+            // placeholder="...."
+            required=""
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="title"
+            className="text-sm font-medium text-black-900 block mb-2 dark:text-black-300 py-1"
+          >
+            Vägbeskrivning
+          </label>
+          <input
+            type="direction"
+            name="direction"
+            id="direction"
+            onChange={(e) => setDirection(e.target.value)}
+            value={directions}
+            className="w-full h-12 rounded-lg bg-white text-gray-600 uppercase font-semibold hover:bg-black-100 transition mb-4"
+            // placeholder="...."
+            required=""
+          />
+        </div>
+
+              <button className="hover:bg-red-dark w-40 h-12 rounded-lg bg-gray-800 text-gray-200 uppercase font-semibold hover:bg-gray-900 transition mb-0 mt-4">Add Location</button>
+
+      </form>
+      {/* </div> */}
     </div>
-    </div>
- 
-  )
-}
+  );
+};
 
 export default AddLocationModal;
