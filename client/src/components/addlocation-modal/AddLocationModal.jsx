b4ae3f074e532447f52a4e3273d6f53807f87bd7
Naelia Salas <naeliasalas@gmail.com>
2022-05-30 07:55:06 +0200

    Add label for header on addlocation form

--- client/src/components/addlocation-modal/AddLocationModal.jsx
+++ client/src/components/addlocation-modal/AddLocationModal.jsx
@@ -1,5 +1,3 @@
-
-
 import React, { useEffect, useState } from "react";
 import "./AddLocation-modal.scss";
 
@@ -12,8 +10,16 @@ const AddLocationModal = () => {
 
     <div className="addLocation-component w-full h-screen flex items-center justify-center my-3">
       {/* <div className="w-medium bg-white rounded shadow-lg p-8 m-4"> */}
-      <form className="bg-[#EDC891] w-80 h-auto rounded-lg pt-8 pb-8 px-6 flex flex-col items-center shadow-xl mb-4 md:flex md:flex-wrap md:justify-between">
+      <form className=" addLocation bg-[#EDC891] w-80 h-auto rounded-lg pt-8 pb-8 px-6 flex flex-col items-center shadow-xl mb-4 md:flex md:flex-wrap md:justify-between">
         <div>
+
+        <label
+            htmlFor="title"
+            className="text-lg font-bold text-gray-900 block mb-2 dark:text-black-300 py-1 text-center"
+          >
+            Lägg till en ny strand
+          </label>
+
           <label
             htmlFor="title"
             className="text-sm font-medium text-gray-900 block mb-2 dark:text-black-300 py-1"
@@ -26,13 +32,15 @@ const AddLocationModal = () => {
             id="title"
             onChange={(e) => setTitle(e.target.value)}
             value={title}
-            className="w-full h-12 rounded-lg bg-white text-gray-600 uppercase font-semibold hover:bg-gray-100 transition mb-4 "
+            className="w-full h-10 rounded-lg bg-white text-gray-600  font-semibold hover:bg-gray-100 transition mb-4 "
             // placeholder="Namn"
             required=""
           />
         </div>
 
         <div>
+
+          
           <label
             htmlFor="title"
             className="text-sm font-medium text-gray-900 block mb-2 dark:text-black-300 py-1"
@@ -45,8 +53,8 @@ const AddLocationModal = () => {
             id="adress"
             onChange={(e) => setAdress(e.target.value)}
             value={adress}
-            className="w-full h-12 rounded-lg bg-white text-gray-600 uppercase font-semibold hover:bg-black-100 transition mb-4"
-            // placeholder="...."
+            className="w-full h-10 rounded-lg bg-white text-gray-600  font-semibold hover:bg-black-100 transition mb-4"
+            // placeholder="Plats"
             required=""
           />
         </div>
@@ -64,13 +72,13 @@ const AddLocationModal = () => {
             id="direction"
             onChange={(e) => setDirection(e.target.value)}
             value={directions}
-            className="w-full h-12 rounded-lg bg-white text-gray-600 uppercase font-semibold hover:bg-black-100 transition mb-4"
-            // placeholder="...."
+            className="w-full h-10 rounded-lg bg-white text-gray-600  font-semibold hover:bg-black-100 transition mb-4"
+            // placeholder="Vägbeskrivning"
             required=""
           />
         </div>
 
-              <button className="hover:bg-red-dark w-40 h-12 rounded-lg bg-gray-800 text-gray-200 uppercase font-semibold hover:bg-gray-900 transition mb-0 mt-4">Lägg till plats</button>
+              <button className="hover:bg-red-dark w-40 h-10 rounded-lg bg-gray-800 text-gray-200 uppercase font-semibold hover:bg-gray-900 transition mb-0 mt-4">Lägg till plats</button>
 
       </form>
       {/* </div> */}
