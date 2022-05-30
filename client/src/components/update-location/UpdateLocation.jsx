f8e06959d9f8d335f3a93a347c561c4e8e85692a
Jonny Norberg <90459832+JonnyNorberg@users.noreply.github.com>
2022-05-30 17:03:13 +0200

    Refactor Profilepage
    Profilepage and components: Update account + Update location refactored.

--- client/src/components/update-location/UpdateLocation.jsx
+++ client/src/components/update-location/UpdateLocation.jsx
@@ -1,24 +1,106 @@
 import React from 'react'
-import './Update-location.scss'; 
+
 
 
 
 const UpdateLocation = () => {
   return (
-<div className="pageWrapper">
 
-	{/* Component here */}
-  
-  <div className="tab w-full overflow-hidden border-t">
-    <input className="absolute opacity-0" id="tab-single-two" type="radio" name="tabs2" />
-      <label className="block p-5 leading-normal cursor-pointer" for="tab-single-two"><i className="icon--size icon--space2 fas fa-swimmer"></i> Trosa havsbad</label>
-        <div className="tab-content overflow-hidden border-l-2 leading-normal">
-          <button className="button-31"> Se plats</button>
-          <button className="button-31"> Ta bort</button>
+  <div className="accordion accordion-flush" id="accordionFlushExample">
+
+        <div className="accordion-item border-l-0 border-r-0 rounded-none bg-white border border-gray-200">
+          <h2 className="accordion-header mb-0" id="flush-headingTwo">
+            <button
+              className="accordion-button
+              collapsed
+              relative
+              flex
+              items-center
+              w-full
+              py-4
+              px-5
+              text-base text-gray-800 text-left
+              bg-white
+              border-0
+              rounded-none
+              transition
+              focus:outline-none"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseTwo"
+              aria-expanded="false"
+              aria-controls="flush-collapseTwo"
+            >
+              <i className="icon--size icon--space2 fas fa-swimmer"></i> Trosa
+              havsbad
+            </button>
+          </h2>
+          <div
+            id="flush-collapseTwo"
+            className="accordion-collapse border-0 collapse"
+            aria-labelledby="flush-headingTwo"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body py-4 px-5">
+              <button className="button-31">
+                {" "}
+                Se plats
+              </button>
+              <button className="button-31">
+                {" "}
+                Ta bort
+              </button>
+            </div>
+          </div>
         </div>
-  </div>
 
-</div>
+        <div className="accordion-item border-l-0 border-r-0 border-b-0 rounded-none bg-white border border-gray-200">
+          <h2 className="accordion-header mb-0" id="flush-headingThree">
+            <button
+              className="accordion-button
+              collapsed
+              relative
+              flex
+              items-center
+              w-full
+              py-4
+              px-5
+              text-base text-gray-800 text-left
+              bg-white
+              border-0
+              rounded-none
+              transition
+              focus:outline-none"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseThree"
+              aria-expanded="false"
+              aria-controls="flush-collapseThree"
+            >
+              <i className="icon--size icon--space2 fas fa-swimmer"></i> Trosa
+              havsbad
+            </button>
+          </h2>
+          <div
+            id="flush-collapseThree"
+            className="accordion-collapse collapse"
+            aria-labelledby="flush-headingThree"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div className="accordion-body py-4 px-5">
+              <button className="button-31">
+                {" "}
+                Se plats
+              </button>
+              <button className="button-31">
+                {" "}
+                Ta bort
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
   )
 }
 
