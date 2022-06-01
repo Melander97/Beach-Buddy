a03198c226b10b5de1acc5fc437732b8fb319b69
Naelia Salas <naeliasalas@gmail.com>
2022-06-01 11:58:10 +0200

    Remove space

--- client/src/components/user-menu/UserMenu.jsx
+++ client/src/components/user-menu/UserMenu.jsx
@@ -11,11 +11,7 @@ const UserMenu = () => {
     <section className="user__menu">
 
       <Link to="/profile"> <i className="icon--space fa-solid fa-user fa-2xl" aria-hidden="true"></i>  </Link>
-
-
       <Link to="/locations"> <i className="icon--space fa fa-map-marker fa-2xl" aria-hidden="true"></i> </Link>
-
-
       <Link to="/add-location"> <i className="icon--space fa-solid fa-circle-plus fa-2xl" aria-hidden="true"></i> </Link>
     
       {/* <button > Log Out </button> */}
