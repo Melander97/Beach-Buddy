c8901d47668cc4555aba799fbfad825fd77a9c0e
melander97 <alexandermelander1@gmail.com>
2022-06-01 17:43:48 +0200

    change a-tag to link menu

--- client/src/components/menu/Menu.jsx
+++ client/src/components/menu/Menu.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import "./Menu.scss";
 import "../update-account/Update-account.scss";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
     <section className="footer-public">
-      <a href="/register">
-        {" "}
-        <i class=" icon--space fa-solid fa-user-plus fa-2xl"></i>{" "}
-      </a>
-      <i class="icon--space fa-solid fa-angle-up fa-2xl"></i>
-      <a href="/add-location">
-        {" "}
+      <Link to="/register">
+        <i className=" icon--space fa-solid fa-user-plus fa-2xl"></i>
+      </Link>
+      <i className="icon--space fa-solid fa-angle-up fa-2xl"></i>
+      <Link to="/add-location">
         <i
-          class="icon--space fa-solid fa-circle-plus fa-2xl"
+          className="icon--space fa-solid fa-circle-plus fa-2xl"
           aria-hidden="true"
         ></i>
-      </a>
+      </Link>
     </section>
   );
 };
