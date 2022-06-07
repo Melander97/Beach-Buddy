c50b38877d475dd7c50e833f6b60be55adb7b040
Niclas-Gustafsson <niclas.gustafsson@chasacademy.se>
2022-06-07 12:38:26 +0200

    remove comments from update location component tied to profile page.

--- client/src/components/update-location/UpdateLocation.jsx
+++ client/src/components/update-location/UpdateLocation.jsx
@@ -52,20 +52,7 @@ const UpdateLocation = ({ location, locationsArray, setUserLocation }) => {
               {" "}
               Ändra
             </Link>
-            {/* <button className="update-btn">Ändra</button> */}
-            {/* <button
-              className="bg-red-400 text-white px-2 py-1"
-              onClick={() => {
-                locationService.deleteLocation(location._id);
-                const filterArray = locationsArray.filter(
-                  (value) => value._id !== location._id
-                );
-                setUserLocation(filterArray);
-              }}
-            >
-              Radera plats
-            </button> */}
-            {/* {console.log(location._id)} */}
+
             <NavLink to={`/location/${location._id}`} className="button-31">
               {" "}
               Se plats
@@ -86,46 +73,6 @@ const UpdateLocation = ({ location, locationsArray, setUserLocation }) => {
           </div>
         </div>
       </div>
-
-      {/* <div className="accordion-item border-l-0 border-r-0 border-b-0 rounded-none bg-white border border-gray-200">
-        <h2 className="accordion-header mb-0" id="flush-headingThree">
-          <button
-            className="accordion-button
-              collapsed
-              relative
-              flex
-              items-center
-              w-full
-              py-4
-              px-5
-              text-base text-gray-800 text-left
-              bg-white
-              border-0
-              rounded-none
-              transition
-              focus:outline-none"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#flush-collapseThree"
-            aria-expanded="false"
-            aria-controls="flush-collapseThree"
-          >
-            <i className="icon--size icon--space2 fas fa-swimmer"></i> Trosa
-            havsbad
-          </button>
-        </h2>
-        <div
-          id="flush-collapseThree"
-          className="accordion-collapse collapse"
-          aria-labelledby="flush-headingThree"
-          data-bs-parent="#accordionFlushExample"
-        >
-          <div className="accordion-body py-4 px-5">
-            <button className="button-31"> Se plats</button>
-            <button className="button-31"> Ta bort</button>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
