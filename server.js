7e2a3eedfc070f1c1df7e96808bf8c43fc7f87ec
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-06-09 02:23:15 +0200

    Add unit testing

--- server.js
+++ server.js
@@ -41,3 +41,4 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
+module.exports = app;
--- test/server.test.js
+++ test/server.test.js
@@ -1,5 +1,9 @@
 var expect = require('chai').expect;
 var request = require('request');
+var supertest = require('supertest');
+var app = require('../server');
+
+
 describe('app rest api testing', () => {
   it('/api/users status code without passing any params', (done) => {
     request('http://localhost:4000/api/users/', (err, result, body) => {
@@ -7,10 +11,19 @@ describe('app rest api testing', () => {
     done();
   });
  });
-  it('api/users/login to show status code 404', (done) => {
+  it('api/users/login to show status code 401', (done) => {
       request('http://localhost:4000/api/users/login', (err, result, body) => {
     expect(result.statusCode).to.equal(401);
     done();
   });
   });
+
+  it('should succeed with proper credentials', function(done){
+    supertest(app)
+    .post('/api/users/login')
+    .send({email:"beach@gmail.com", password: "password"})
+    .expect(200, done)
+    })
+
 });
+
